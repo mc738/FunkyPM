237a229182c74ebfe0debf91148926b3564a88bc
Max <me@mclifford.dev>
2025-05-19 22:20:54 +0100

    feat(ops): Added Sync.fs

--- FunkyPM/V1/Operations/Sync.fs
+++ FunkyPM/V1/Operations/Sync.fs
@@ -1,8 +1,17 @@
 ﻿namespace FunkyPM.V1.Operations
 
+open System.IO
+
 module Sync =
     
+    let withFile (path: string) (fn: MemoryStream -> unit) =
+        use fs = File.OpenRead(path)
+        
+        use ms = new MemoryStream()
+        
+        fs.CopyTo(ms)
+        
+        ms.Position <- 0L
+        
+        fn ms
     
-    
-    ()
-
