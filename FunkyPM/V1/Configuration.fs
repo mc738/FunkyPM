d4e60164ba8f5828fd71cf99bb4a34b6ade9a73b
Max <me@mclifford.dev>
2025-05-19 22:07:50 +0100

    feat(configuration): Added Configuration.fs
    Added Configuration.fs and started working on implementation

---
+++ FunkyPM/V1/Configuration.fs
@@ -0,0 +1,16 @@
+﻿namespace FunkyPM.V1
+
+module Configuration =
+
+    [<RequireQualifiedAccess>]
+    type ConfigurationValue =
+        | Provided of string
+        | EnvironmentVariable of string
+
+        static member Deserialize(value: string) =
+            match value with
+            | _ when value.StartsWith("$") -> value.Substring(1) |> EnvironmentVariable
+            | _ -> Provided value
+
+
+    type FunkyPMConfiguration = { StorePath: string }
