0302c1a62082b696306c00dde167e1ae936877ff
Max <me@mclifford.dev>
2025-05-21 22:41:41 +0100

    feat(ops): Work on building up structure

--- FunkyPM/V1/Operations/Common.fs
+++ FunkyPM/V1/Operations/Common.fs
@@ -6,7 +6,7 @@ module Common =
     type Context =
         {
             ItemGroups: string
-      
+            Categories: string
         }
     
     let splitPathValue (path: string) = path.Split([|'/'|])
