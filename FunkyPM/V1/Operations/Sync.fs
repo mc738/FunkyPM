babf575b55a5c2b7b8f9e0123f9ab03908fe8ca2
Max <me@mclifford.dev>
2025-05-21 21:48:36 +0100

    feat(core): Added metadata extraction

--- FunkyPM/V1/Operations/Sync.fs
+++ FunkyPM/V1/Operations/Sync.fs
@@ -4,6 +4,10 @@ open System.IO
 
 module Sync =
     
+    
+    let getMetaData (lines: string list) =
+        FDOM.Core.Parsing.Parser.ExtractMetadata lines
+    
     let withFile (path: string) (fn: MemoryStream -> unit) =
         use fs = File.OpenRead(path)
         
