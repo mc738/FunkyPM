e5f712ae5512f47d69f38c07e2c8b5d47ebe03bf
Max <me@mclifford.dev>
2025-05-21 22:29:54 +0100

    feat(op): Work on building context

--- FunkyPM/V1/Operations/Common.fs
+++ FunkyPM/V1/Operations/Common.fs
@@ -1,2 +1,11 @@
-﻿module FunkyPM.V1.Operations.Common
+﻿module FunkyPM.V1.Operations
 
+module Common =
+    
+    let splitPathValue (path: string) = path.Split([|'/'|])
+    
+    let substituteValue (ctx) (value: string) =
+        
+        
+        ()
+    
